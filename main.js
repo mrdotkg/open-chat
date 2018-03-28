bf28e7f3cfaf003a3ead13406fcd9092fd37f3ad
Kumar Gaurav <grv.rkg@gmail.com>
2018-03-28 10:30:43 +0530

    ui issues fixed features add complete

--- main.js
+++ main.js
@@ -1,27 +1,55 @@
 $(function(){
-  $("i.fa-envelope-open").hide();
+  $( document ).ready(function() {
+   
+    $(".action").hide();
   
-  $(".heading-compose").click(function() {
-    $(".side-two").css({
-      "left": "0"
+    $(".heading-compose").click(function() {
+      $(".side-two").css({
+        "left": "0"
+      });
     });
-  });
 
-  $(".newMessage-back").click(function() {
-    $(".side-two").css({
-      "left": "-100%"
+    $(".newMessage-back").click(function() {
+      $(".side-two").css({
+        "left": "-100%"
+      });
     });
-  });
-
-  $(".side-two .sideBar-body").hover(
-    function() {
-      $(this).find("i.fa-envelope-open").show();
-    },
-    function() {
-      $(this).find("i.fa-envelope-open").hide();
-    }
-
-);
-  
     
+    $(".toggle-profile").on("click", function(){
+      if ($(this).hasClass('open')){
+        
+        // console.log('closing');
+        $(".conversation").removeClass('col-sm-6 ').addClass('col-sm-8 ');
+        $(".side.left").removeClass('col-sm-3 ').addClass('col-sm-4');
+        $(this).find('i').removeClass('fa-chevron-circle-right').addClass('fa-chevron-circle-left');
+        $(".side.right").hide();
+        $(this).removeClass('open');
+      
+      }else {
+        
+        // console.log('opening');
+        $(".conversation").removeClass('col-sm-8 ').addClass('col-sm-6 ');
+        $(".side.left").removeClass('col-sm-4 ').addClass('col-sm-3 ');
+        $(this).find('i').removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-right');
+        $(".side.right").show();
+        $(this).addClass('open');
+      }
+    });
+    
+
+    $(".newMessage-back").click(function() {
+      $(".side-two").css({
+        "left": "-100%"
+      });
+    });
+
+    $(".side-two .sideBar-body").hover(
+      function() {
+        $(this).find("i.action").show();
+      },
+      function() {
+        $(this).find("i.action").hide();
+      }
+    );
+  }); 
 })
